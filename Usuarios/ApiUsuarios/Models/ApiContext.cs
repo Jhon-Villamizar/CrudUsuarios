62493071f0eecfc9628685d6a75217dbd666fc32
Jhon Villamizar <jhonvio@hotmail.com>
2019-05-25 12:25:11 -0500

    creacion de api en backend .net framework

---
+++ Usuarios/ApiUsuarios/Models/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiUsuarios.Models
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext() : base("ApiUsuariosConnection")
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Usuarios/ApiUsuarios/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ApiUsuarios.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string nombre { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string email { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string password { get; set; }
+    }
+}
